eac849fad0184a541256e6a5300dddcbe95c6057
sanabeel <s.a.nabeel@gmail.com>
2020-07-26 20:01:27 +0530

    End of section6

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -6,7 +6,16 @@ namespace Core.Specifications
 {
     public interface ISpecification<T>
     {
+        // give expression in Iqueryable manner which will get executed in db
+        // Here criteria's return vale will be boolean (where clause). ie whether a data to be selected or not
         Expression<Func<T, bool>> Criteria { get; } 
+        // here for this express a list of navigation data in the format of basic data type 'object'
         List<Expression<Func<T, object>>> Includes { get; }
+        // Here we want to add expression for sorting, so output will be an object ( in sorted manner , that does nt affect expression)
+        Expression<Func<T, object>> OrderBy {get;}
+        Expression<Func<T, object>> OrderByDescending {get;}
+        int Take { get;}
+        int Skip { get; }
+        bool IsPagingEnabled {get;}
     }
 }
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,17 +6,43 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+            :base( x=> 
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue||x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue||x.ProductTypeId == productParams.TypeId)
+                )
         {
-            AddInclude( x => x.ProductType );
-            AddInclude( x => x.ProductBrand );
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), 
+                productParams.PageSize);
+            //AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
-        public ProductsWithTypesAndBrandsSpecification(int id) 
+        public ProductsWithTypesAndBrandsSpecification(int id)
         : base(x => x.Id == id)
         {
-            AddInclude( x => x.ProductType );
-            AddInclude( x => x.ProductBrand );
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
         }
     }
 }
