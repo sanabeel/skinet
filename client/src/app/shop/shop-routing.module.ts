0e2f4ab39f2ad8de2697d3d9bc3bb8088b4c5b8f
sanabeel <s.a.nabeel@gmail.com>
2020-08-01 19:54:47 +0530

    end of section 10

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
+const route: Routes = [
+  {path: '', component: ShopComponent},
+  {path: ':id', component: ProductDetailsComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(route)
+  ], exports:[
+    RouterModule
   ]
 })
 export class ShopRoutingModule { }
